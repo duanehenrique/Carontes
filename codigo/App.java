395b90bbea9bf3149bad702dcd9f12c9e9d01399
Duane Henrique <duanealves@ymail.com>
2023-12-14 08:34:57 -0300

    Correção da instanciação do HashMap

--- codigo/App.java
+++ codigo/App.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
@@ -115,11 +116,11 @@ public class App {
             String tipoVeiculo = tiposVeiculo[random.nextInt(tiposVeiculo.length)];
 
             // Mapa de custos de manutenção
-        Map<String, Double> custosManutencao = new HashMap<>();
-        custosManutencao.put("Carro", 500.0);
-        custosManutencao.put("Van", 750.0);
-        custosManutencao.put("Furgao", 1000.0);
-        custosManutencao.put("Caminhao", 1500.0);
+            Map<String, Double> custosManutencao = new HashMap<>();
+            custosManutencao.put("Carro", 500.0);
+            custosManutencao.put("Van", 750.0);
+            custosManutencao.put("Furgao", 1000.0);
+            custosManutencao.put("Caminhao", 1500.0);
 
             // Lista de nomes de motoristas
             String[] nomesMotoristas = { "João", "Maria", "José", "Ana", "Pedro", "Sandra", "Carlos", "Laura",
